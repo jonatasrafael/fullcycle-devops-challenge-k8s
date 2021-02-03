a396a0afa6b46033512a85d908f64b6dc387036a
Jonatas Rafael Carneiro Ribeiro <jonatasrafael@jonatasrafael.com>
2021-02-03 12:23:27 -0300

    Teste erro no test

--- 3-go/main_test.go
+++ 3-go/main_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestGreeting(t *testing.T) {
 	resultado := greeting("teste")
-	if resultado != "<b>teste</b>" {
+	if resultado != "<b>test</b>" {
 		t.Errorf("Texto esperado: %s, obtido: %s", "<b>teste</b>", resultado)
 	}
 }
